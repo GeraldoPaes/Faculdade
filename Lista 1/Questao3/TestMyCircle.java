be4186076fb77c560f3d1497d2476411e22110f8
RavenbornIV <41174251+RavenbornIV@users.noreply.github.com>
2018-10-09 17:25:18 -0300

    Update TestMyCircle.java

--- Lista 1/Questao3/TestMyCircle.java
+++ Lista 1/Questao3/TestMyCircle.java
@@ -7,6 +7,22 @@ public class TestMyCircle {
 		MyCircle c2 = new MyCircle(p1, 3);
 		
 		System.out.println(c1.getCenterX() + " " + c1.getCenterY() + " " + c1.getCenter());
+		
+		c1.setCenter(p1);
+		c1.setRadius(4);
+		c1.setCenterXY(3, 3);
+		
+		System.out.println(c1.getArea());
+		System.out.println(c2.toString());
+		
+		MyCircle c3 = new MyCircle(1, 1, 1);
+		MyCircle c4 = new MyCircle(1, 1, 1);
+		
+		System.out.println("\n" + c3.equals(c4));
+		
+		c4.setCenterXY(3, 3);
+		
+		System.out.println(c3.equals(c4));
 	}
 
 }
