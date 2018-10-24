4b790dac02f98de73442668122775379c7a4bc58
RavenbornIV <41174251+RavenbornIV@users.noreply.github.com>
2018-10-23 22:36:32 -0300

    Update Main.java

--- Lista 2/Exercicico 1/Main.java
+++ Lista 2/Exercicico 1/Main.java
@@ -28,7 +28,7 @@ public class Main {
 		
 		Territorio t = new Territorio(tam, nome1, nome2);
 		
-		System.out.println("\n");
+		System.out.print("\n");
 		
 		for(int i = 0; i < tam; i++) {
 			for(int j = 0; j < tam; j++) {
@@ -37,16 +37,83 @@ public class Main {
 			System.out.print("\n");
 		}
 		
+		System.out.print("\n");
+		
 		int fim = 0;
 		
 		while(fim == 0) {
+			String move;
+			int result;
 			int jog1, jog2;
 			jog1 = 1;
 			jog2 = 1;
 			
 			while(jog1 == 1) {
 				System.out.print("Jogada de "  + t.getP1Name() + " (w, a, s, d): ");
+				move = scan.next();
+				result = t.movePlayer1(move);
+				
+				if(result == 0) {
+					jog1 = 0;
+				}
+				else if(result == -1) {
+					System.out.println("\nComando inválido, insira uma das teclas indicadas.");
+				}
+				else {
+					System.out.println("\n" + t.getP1Name() + " colidiu com alguma coisa! A vitória é de " + t.getP2Name() + "!");
+					jog1 = 0;
+					fim = 1;
+				}
 			}
+			
+			if(fim == 1) {
+				break;
+			}
+			
+			System.out.print("\n");
+			
+			for(int i = 0; i < tam; i++) {
+				for(int j = 0; j < tam; j++) {
+					System.out.print(t.getPoint(i, j) + " ");
+				}
+				System.out.print("\n");
+			}
+			
+			System.out.print("\n");
+			
+			while(jog2 == 1) {
+				System.out.print("Jogada de "  + t.getP2Name() + " (w, a, s, d): ");
+				move = scan.next();
+				result = t.movePlayer2(move);
+				
+				if(result == 0) {
+					jog2 = 0;
+				}
+				else if(result == -1) {
+					System.out.println("\nComando inválido, insira uma das teclas indicadas.");
+				}
+				else {
+					System.out.println("\n" + t.getP2Name() + " colidiu com alguma coisa! A vitória é de " + t.getP1Name() + "!");
+					jog2 = 0;
+					fim = 1;
+				}
+			}
+			
+			if(fim == 1) {
+				break;
+			}
+			
+			System.out.print("\n");
+			
+			for(int i = 0; i < tam; i++) {
+				for(int j = 0; j < tam; j++) {
+					System.out.print(t.getPoint(i, j) + " ");
+				}
+				System.out.print("\n");
+			}
+			
+			System.out.print("\n");
+			
 		}
 
 	}
